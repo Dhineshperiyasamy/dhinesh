c43f3cdf12d0983618dda41889084a1685ebee05
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-01-19 16:09:13 +0530

    Create po.c

---
+++ po.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if(n>0)
+{
+if(n=0)
+printf("enter the posite number");
+else
+printf("enter the negative number");
+}
+else
+printf("enter the number");
+retuern 0;
+}
