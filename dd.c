bd453a1e24f496f8260ed070d76feac6e4abd771
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-01-21 04:43:12 +0000

    Create dd.c

---
+++ dd.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+void main()
+{
+int a,b,c[40],i;
+int sum=0;
+printf("\n enter the number");
+scanf("%d%d",&a,&b);
+for(i=0;i<b;i++)
+{
+scanf("%b",&j[i]);
+}
+for(i=0;i<c;i++)
+{
+sum=sum+j[i];
+}
+printf("%b",sum);
+}
