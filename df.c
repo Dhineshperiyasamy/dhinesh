4d490533c9bb9deb38c2cfdd9701d0f9ef76f727
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-01-21 04:48:57 +0000

    Create df.c

---
+++ df.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int a,b,c;
+printf(" enter the number");
+scanf("%d",&a);
+printf("enter the value of power");
+scanf("%d",&b);
+y=power(a^b);
+printf("%d",c);
+}
