465212dd8b80ce71f33d8d1c590d4931ed21ec13
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-01-21 06:18:20 +0000

    Rename da.c to dp.c

---
+++ dp.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+int main()
+{
+int number,result=0
+printf("Enter a three digit integer: ");
+scanf("%d", &number);
+if(result == number)
+printf("%d is an Armstrong number.",number);
+else
+printf("%d is not an Armstrong number.",number);
+return 0;
+}
