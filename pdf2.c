95d98f8879314bd015e5bb71ec2fd6f30f8a287f
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-01-24 15:54:11 +0530

    Create pdf2.c

---
+++ pdf2.c
@@ -0,0 +1,13 @@
+# #include<stdio.h>
+void main()
+{
+int j,i,k,add;
+printf("Enter the numbers");
+scanf("%d",&i);
+printf("Enter the starting number");
+scanf("%d",&j);
+printf("Enter the difference");
+scanf("%d",&k);
+add=j+i+k;
+printf("%d",add);
+}
