508dd428b6248c694be24280498dbfb391f0a850
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-01-24 15:57:24 +0530

    Create pdf3.c

---
+++ pdf3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a,w;
+int arr[10];
+printf("enter the total number ");
+scanf("%d",&w);
+for(i=0;i<w;++i)
+{
+printf("enter the number %d";i+1)
+scanf("%d",&arr[i]);
+for(i=1;i<w;++i)
+{
+if(arr[0]<arr[a])
+arr[0]=arr[a];
+}
+printf("maximum element=%d",arr[a]);
+}
+}
