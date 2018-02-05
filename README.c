a815729d9e1cc4f2847b3c3afbceeef69be2ff67
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-02-05 16:03:30 +0530

    Update README.c

--- README.c
+++ README.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-int x,z;
+int a,b;
 printf("\nenter two numbers:");
 scanf("%d%d",&x,&z);
 printf("\nnumber before swapping are:%d%d",x,z);
-x=x^z;
-z=x^z;
-x=x^z;
+a=a^c;
+c=a^c;
+a=a^c;
 printf("\nnumbers after swapping:%d%d",x,z);
 return 0;
 }
