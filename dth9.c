f17fc63e2962e9997046dfbf78f4d500ec840241
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-02-09 16:27:11 +0530

    Update and rename dca4.c to dth9.c

---
+++ dth9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+ int b;
+ printf("enter the no.");
+ scanf("%d",&b);
+ if(a<10)
+ {
+ 	printf("yes with in the range %d",b);
+ }
+ else
+ {
+ 	printf("not with in the range");
+ }
+	return 0;
+}
+
+
