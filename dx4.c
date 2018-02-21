e6efc53c05ab8361c2064fcdd03e8990dcb95f92
Dhineshperiyasamy <31499254+Dhineshperiyasamy@users.noreply.github.com>
2018-02-21 16:35:42 +0530

    Create dx4.c

---
+++ dx4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+ 
+int main() {
+	int n;
+	scanf("%d",&n);
+	switch(n){
+		case 1:
+		printf("\n one");
+		break;
+		case 2:
+		printf("\n Two");	break;
+		case 3:
+		printf("\n Three");	break;
+		case 4:
+		printf("\n Four");	break;
+		case 5:
+		printf("\n Five");	break;
+		case 6:
+		printf("\n Six");	break;
+		case 7:
+		printf("\n Seven");	break;
+		case 8:
+		printf("\n Eight");	break;
+		case 9:
+		printf("\n Nine");	break;
+		case 10:
+		printf("\n Ten");	break;
+ 
+	}
+	return 0;
+}
